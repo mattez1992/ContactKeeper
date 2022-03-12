5b0bfce6f6e41d40f2bf8556244f77c7ce2a9e12
DESKTOP-1CL2VLB\matte <matte_920319@live.se>
2022-03-12 18:21:24 +0100

    created delete contact method

---
+++ client/src/Components/contacts/ContactItem.js
@@ -0,0 +1,57 @@
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import ContactContext from "../../Context/contact/contactContext";
+
+function ContactItem({ contact }) {
+  const { id, name, email, phone, type } = contact;
+  const contactContext = useContext(ContactContext);
+  const onDelete = (e) => {
+    contactContext.deleteContact(id);
+  };
+  const onEdit = (e) => {};
+  return (
+    <div className="card bg-light">
+      <h3 className="text-primary text-left">
+        {name}{" "}
+        <span
+          style={{ float: "right" }}
+          className={
+            "badge " +
+            (type === "professional" ? "badge-success" : "badge-primary")
+          }
+        >
+          {/* This will capitalise the irst letter of type */}
+          {type.charAt(0).toUpperCase() + type.slice(1)}
+        </span>
+      </h3>
+      <ul className="list">
+        {email && (
+          <li>
+            <i className="fas fa-envelope-open" />
+            &nbsp;{email}
+          </li>
+        )}
+        {phone && (
+          <li>
+            <i className="fas fa-phone" />
+            &nbsp;{phone}
+          </li>
+        )}
+      </ul>
+      <p>
+        <button className="btn btn-dark btn-sm" onClick={onEdit}>
+          Edit
+        </button>
+        <button className="btn btn-danger btn-sm" onClick={onDelete}>
+          Delete
+        </button>
+      </p>
+    </div>
+  );
+}
+
+ContactItem.propTypes = {
+  contact: PropTypes.object.isRequired,
+};
+
+export default ContactItem;
--- client/src/Components/pages/Home.js
+++ client/src/Components/pages/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
-
+import Contacts from "../contacts/Contacts";
+import ContactForm from "../contacts/ContactForm";
 export const Home = () => {
   return (
-    <div>
-      <h1>This Is Home</h1>
+    <div className="grid-2">
+      <div>
+        <ContactForm />
+      </div>
+      <div>
+        <Contacts />
+      </div>
     </div>
   );
 };
